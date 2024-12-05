0872a4c39112cad1bcdd9fbe052fe3f398c6f9fd
bvr <20733579+bvr33@users.noreply.github.com>
2024-12-05 05:54:33 +0100

    init

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using AdventOfCode2024.src.day_1;
+using AdventOfCode2024.src.day_2;
+using AdventOfCode2024.src.day_3;
+using AdventOfCode2024.src.day_4;
+using AdventOfCode2024.src.day_5;
+
+// Day_1_1.Run();
+// Day_1_2.Run();
+// Day_2_1.Run();
+// Day_2_2.Run();
+// Day_3_1.Run();
+// Day_3_2.Run();
+// Day_4_1.Run();
+// Day_4_2.Run();
+Day_5_1.Run();
