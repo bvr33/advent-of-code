3e75a0d0ace95670f7e253c2c1650521bf45bbb2
bvr <20733579+bvr33@users.noreply.github.com>
2024-12-05 05:56:41 +0100

    add Day4_1 solve

---
+++ AdventOfCode2024/src/day_4/Day_4_1.cs
@@ -0,0 +1,86 @@
+using AdventOfCode2024.src.utils;
+
+namespace AdventOfCode2024.src.day_4
+{
+    public static class Day_4_1
+    {
+        private static readonly string searchedValue = "XMAS";
+        public static void Run()
+        {
+            string[] inputFileLines = InputReader.GetFileLinesRecursiveToSrcFolder("day_4/input.txt").ToArray();
+
+            char[,] char2dArray = Get2DCharArray(inputFileLines);
+
+            int soughtValueCount = 0;
+            for (int i = 0; i < char2dArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < char2dArray.GetLength(1); j++)
+                {
+                    soughtValueCount += HasHorizontalValue(char2dArray, i, j) + HasVerticalValue(char2dArray, i, j) + HasDiagonalsValue(char2dArray, i, j);
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine(soughtValueCount);
+        }
+
+        private static int HasHorizontalValue(char[,] char2dArray, int i, int j)
+        {
+            if (i + 3 >= char2dArray.GetLength(0)) return 0;
+
+            string valueToCheck = $"{char2dArray[i, j]}{char2dArray[i + 1, j]}{char2dArray[i + 2, j]}{char2dArray[i + 3, j]}";
+            if (IsSoughtValue(valueToCheck)) return 1;
+
+            return 0;
+        }
+
+        private static int HasVerticalValue(char[,] char2dArray, int i, int j)
+        {
+            if (j + 3 >= char2dArray.GetLength(1)) return 0;
+
+            string valueToCheck = $"{char2dArray[i, j]}{char2dArray[i, j + 1]}{char2dArray[i, j + 2]}{char2dArray[i, j + 3]}";
+            if (IsSoughtValue(valueToCheck)) return 1;
+
+            return 0;
+        }
+
+        private static int HasDiagonalsValue(char[,] char2dArray, int i, int j)
+        {
+            if (i + 3 >= char2dArray.GetLength(0) || j + 3 >= char2dArray.GetLength(1)) return 0;
+
+            string[] valuesToCheck = [
+                $"{char2dArray[i, j]}{char2dArray[i + 1, j + 1]}{char2dArray[i + 2, j + 2]}{char2dArray[i + 3, j + 3]}",
+                $"{char2dArray[i+3, j]}{char2dArray[i+2,j+1]}{char2dArray[i+1,j+2]}{char2dArray[i,j+3]}"
+            ];
+
+            int valueCount = 0;
+            foreach (string value in valuesToCheck)
+            {
+                if (IsSoughtValue(value)) valueCount++;
+            }
+
+            return valueCount;
+        }
+
+        private static bool IsSoughtValue(string valueToCheck)
+        {
+            string reversedSearchedValue = new(searchedValue.Reverse().ToArray());
+            return searchedValue == valueToCheck || reversedSearchedValue == valueToCheck;
+        }
+
+        private static char[,] Get2DCharArray(string[] inputFileLines)
+        {
+            char[,] charArray = new char[inputFileLines.Length, inputFileLines[0].Length];
+
+            for (int i = 0; i < inputFileLines.Length; i++)
+            {
+                for (int j = 0; j < inputFileLines[0].Length; j++)
+                {
+                    charArray[i, j] = inputFileLines[i][j];
+                }
+            }
+
+            return charArray;
+        }
+    }
+}
